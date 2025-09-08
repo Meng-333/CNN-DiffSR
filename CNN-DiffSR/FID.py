ba743714fea0dbe3516df08d4462f27c16e2f0a8
Mengfanen <64727746+Meng-333@users.noreply.github.com>
2025-09-08 20:04:33 +0800

    Update FID.py

--- CNN-DiffSR/FID.py
+++ CNN-DiffSR/FID.py
@@ -14,6 +14,6 @@ hr_path = "/data/mfe/mfe_diffsr/dataset/Test/AID_Test"
 paths = [rslt_path, hr_path]
 fid_score = calc_fid(paths)
 print("- SR_FID : {:.5f}".format(fid_score))
-print16 = "- SR_FID : {:.5f}".format(fid_scor)
+print16 = "- SR_FID : {:.5f}".format(fid_score)
 
 
